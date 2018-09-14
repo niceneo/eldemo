8a5f5585ada396dbbb38533cdf6cecd4d4e898de
niceneo <niceneo666@gmail.com>
2018-09-14 18:58:30 +0800

    menu init config file

---
+++ src/config/menu-config.js
@@ -0,0 +1,23 @@
+export default [{
+    name: '基础',
+    id: 'basic',
+    icon: 'el-icon-d-arrow-right',
+    sub: [{
+        name: 'Layout 布局',
+        componentName: 'BasicLayout'
+    },{
+        name: 'Container 布局容器',
+        componentName: 'BasicContainer'
+    }]
+}, {
+    name: 'Form',
+    id: 'form',
+    icon: 'el-icon-d-arrow-right',
+    sub: [{
+        name: 'Radio 单选框',
+        componentName: 'ForRadio'
+    },{
+        name: 'CheckBox 多选框',
+        componentName: 'FormCheckBox'
+    }]
+}]
